98f0e71299a930a2d05dde9730b2388d1d5ec364
MichaelCalbay <vjc0781@autuni.ac.nz>
2023-07-18 12:48:10 +1200

    Test Commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fiserv Onboarding',
+      title: 'Test Commit',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
