3ace5206ea7df48b7faf7a3c49a1605b7e5f8c88
MichaelCalbay <vjc0781@autuni.ac.nz>
2023-07-27 01:16:48 +1200

    refactor(hardcoded values): get values from db

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fiservonboardingexp/widgets/custom_text_box.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +19,8 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final currentUser = FirebaseAuth.instance.currentUser!;
+  final userCollection = FirebaseFirestore.instance.collection('User');
   /*This will show an alert dialogue which will enable us to edit
   information about ourselves which will be saved into
   our database later.*/
@@ -71,24 +75,14 @@ class _ProfilePageState extends State<ProfilePage> {
               'Save',
               style: TextStyle(color: Colors.white),
             ),
-            onPressed: () {
-              setState(() {
-                if (newValue.trim().isNotEmpty) {
-                  if (fieldName == "Interesting Facts") {
-                    interestingFacts = newValue;
-                  } else if (fieldName == "Hobbies") {
-                    hobbies = newValue;
-                  } else {
-                    futureSelf = newValue;
-                  }
-                }
-              });
-              Navigator.of(context).pop();
-            },
-          )
+            onPressed: () => Navigator.of(context).pop(newValue),
+          ),
         ],
       ),
     );
+    if (newValue.isNotEmpty) {
+      await userCollection.doc(currentUser.uid).update({fieldName: newValue});
+    }
   }
 
   @override
@@ -103,123 +97,143 @@ class _ProfilePageState extends State<ProfilePage> {
         ),
         backgroundColor: Colors.black,
       ),
-      body: ListView(
-        children: [
-          //This is just used for spacing between elements.
-          const SizedBox(height: 30),
-          //User profile picture, will be pulled from firebase.
-          //For now, we will use a placeholder.
-          IconButton(
-            icon: const Icon(Icons.person),
-            color: const Color(0xFFFF6600),
-            iconSize: 50,
-            onPressed: () {
-              debugPrint('Edit profile avatar/picture to be implemented...');
-            },
-          ),
-          //User fullname.
-          //Using a place holder for now.
-          const SizedBox(height: 10),
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection("User")
+            .doc(currentUser.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final userData = snapshot.data!.data() as Map<String, dynamic>;
 
-          Text(
-            'Michael Calbay',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.grey.shade700,
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),
-          ),
+            return ListView(
+              children: [
+                //This is just used for spacing between elements.
+                const SizedBox(height: 30),
+                //User profile picture, will be pulled from firebase.
+                //For now, we will use a placeholder.
+                IconButton(
+                  icon: const Icon(Icons.person),
+                  color: const Color(0xFFFF6600),
+                  iconSize: 50,
+                  onPressed: () {
+                    debugPrint(
+                        'Edit profile avatar/picture to be implemented...');
+                  },
+                ),
+                //User fullname.
+                //Using a place holder for now.
+                const SizedBox(height: 10),
 
-          const SizedBox(height: 50),
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0),
-            child: Text(
-              'About Me:',
-              style: TextStyle(
-                color: Colors.grey.shade700,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),
-            ),
-          ),
+                Text(
+                  '${userData['firstName']} ${userData['lastName']}',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey.shade700,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
 
-          //Interesting fact field
-          CustomTextBox(
-            text: interestingFacts,
-            fieldName: 'Interesting Facts',
-            onPressed: () => editField('Interesting Facts'),
-          ),
+                const SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    'About Me:',
+                    style: TextStyle(
+                      color: Colors.grey.shade700,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
 
-          //Hobbies
-          CustomTextBox(
-            text: hobbies,
-            fieldName: 'Hobbies',
-            onPressed: () => editField('Hobbies'),
-          ),
+                //Interesting fact field
+                CustomTextBox(
+                  text: userData['Interesting Facts'],
+                  fieldName: 'Interesting Facts',
+                  onPressed: () => editField('Interesting Facts'),
+                ),
 
-          //Future self
-          CustomTextBox(
-            text: futureSelf,
-            fieldName: 'Future Self',
-            onPressed: () => editField('Future Self'),
-          ),
+                //Hobbies
+                CustomTextBox(
+                  text: userData['Hobbies'],
+                  fieldName: 'Hobbies',
+                  onPressed: () => editField('Hobbies'),
+                ),
 
-          const SizedBox(height: 50),
+                //Future self
+                CustomTextBox(
+                  text: userData['Future Self'],
+                  fieldName: 'Future Self',
+                  onPressed: () => editField('Future Self'),
+                ),
 
-          /*Submit button will generate a pdf
+                const SizedBox(height: 50),
+
+                /*Submit button will generate a pdf
           Then send that PDF to colleagues*/
-          /*I will set send introduction button to work only once.
+                /*I will set send introduction button to work only once.
           After it had been pressed at least 1 time, the button will either disappear
           or will become a logout button instead
           I will still allow users to edit their "about me".*/
-          //To be implemented later.
-          sendButtonPressed == false
-              ? Padding(
-                  padding: const EdgeInsets.only(
-                    left: 120.0,
-                    right: 120.0,
-                  ),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                        const Color(0xFFFF6600),
+                //To be implemented later.
+                sendButtonPressed == false
+                    ? Padding(
+                        padding: const EdgeInsets.only(
+                          left: 120.0,
+                          right: 120.0,
+                        ),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xFFFF6600),
+                            ),
+                          ),
+                          onPressed: () {
+                            debugPrint('Submit button pressed...');
+                            //After Send Introduction has been sent once, it will turn into a Logout button.
+                            setState(() {
+                              sendButtonPressed = true;
+                            });
+                          },
+                          child: const Text('Send Introduction'),
+                        ),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.only(
+                          left: 120.0,
+                          right: 120.0,
+                        ),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.grey.shade600,
+                            ),
+                          ),
+                          onPressed: () {
+                            debugPrint('Logout button pressed...');
+                            //For testing purposes, we will revert back to Send Introduction button.
+                            //Once fully implemented, it will be a Logout button permanently.
+                            setState(() {
+                              sendButtonPressed = false;
+                            });
+                          },
+                          child: const Text('Logout'),
+                        ),
                       ),
-                    ),
-                    onPressed: () {
-                      debugPrint('Submit button pressed...');
-                      //After Send Introduction has been sent once, it will turn into a Logout button.
-                      setState(() {
-                        sendButtonPressed = true;
-                      });
-                    },
-                    child: const Text('Send Introduction'),
-                  ),
-                )
-              : Padding(
-                  padding: const EdgeInsets.only(
-                    left: 120.0,
-                    right: 120.0,
-                  ),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                        Colors.grey.shade600,
-                      ),
-                    ),
-                    onPressed: () {
-                      debugPrint('Logout button pressed...');
-                      //For testing purposes, we will revert back to Send Introduction button.
-                      //Once fully implemented, it will be a Logout button permanently.
-                      setState(() {
-                        sendButtonPressed = false;
-                      });
-                    },
-                    child: const Text('Logout'),
-                  ),
-                ),
-        ],
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error${snapshot.error}'),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
