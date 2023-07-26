67c52adc2b065b7f4ddfa74165435736573514fd
MichaelCalbay <vjc0781@autuni.ac.nz>
2023-07-26 23:50:06 +1200

    Merged Development Branch into MichaelC

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:fiservonboardingexp/config/app_router.dart';
-import 'package:fiservonboardingexp/screens/home_screen.dart';
-import 'package:fiservonboardingexp/screens/profile_page.dart';
+import 'package:fiservonboardingexp/screens/Login_page.dart';
 import 'package:flutter/material.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fiservonboardingexp/firebase_options.dart';
 
 // Import class from another file
-import 'login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
