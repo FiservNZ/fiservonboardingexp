83432ba23d6d4b74d5d8df0e5959c78d6b7a8c2d
MichaelCalbay <111832324+MichaelCalbay@users.noreply.github.com>
2023-07-27 12:39:52 +1200

    Update main.dart
    Modifying main to enable developers to test without logging in.

--- lib/main.dart
+++ lib/main.dart
@@ -7,5 +7,22 @@ import 'package:fiservonboardingexp/firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const LoginApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fiserv Onboarding',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //Change home field to the page you want to test.
+      home: const LoginApp(),
+    );
+  }
 }
