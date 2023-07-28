2e8a11ac5bf15b3c165f6b65036f56afbe0a5dea
rlathdud1972 <rlathdud1972@gmail.com>
2023-07-28 17:18:54 +1200

    Fixed show teaser on first launch, and doesn't show teaser on subsequent launches

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:fiserv/page/NavAppOverlay.dart';
 import 'package:flutter/material.dart';
 import 'page/teaser.dart';
 import 'page/profile_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:after_layout/after_layout.dart';
 
-void main() {
-  runApp(MyApp());
+int? initScreen;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  initScreen = await preferences.getInt('initScreen');
+  await preferences.setInt('initScreen', 1);
+    runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,8 +24,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TeaserScreen(),
+      initialRoute: initScreen == 0 || initScreen == null ? 'teaser' : 'home',
+      routes: {
+        'home' : (context) => MyHomePage(),
+        'teaser': (context) => TeaserScreen(),
+      },
     );
   }
 }
-
