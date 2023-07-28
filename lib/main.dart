e78781e21dbc13a1bfdcf40436049f2319d04913
MichaelCalbay <vjc0781@autuni.ac.nz>
2023-07-28 15:42:11 +1200

    fix: fix main to enable devs to test without logging in

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,25 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fiservonboardingexp/firebase_options.dart';
 
-// Import class from another file
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const LoginApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fiserv Onboarding',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //Change home field to the page you want to test.
+      home: const LoginApp(),
+    );
+  }
 }
