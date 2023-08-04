9396d75e0eb3e60a2fe982b7dd0f62fbdcfaf1b0
MichaelCalbay <vjc0781@autuni.ac.nz>
2023-08-04 19:59:23 +1200

    refactor: clean .dart files and main

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fiservonboardingexp/firebase_options.dart';
 
-int? initScreen;
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -23,11 +21,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // initialRoute: initScreen == 0 || initScreen == null ? 'teaser' : 'home',
-      // routes: {
-      //   'home': (context) => const LoginApp(),
-      //   'teaser': (context) => const TeaserScreen(),
-      // },
       home: const LoginApp(),
     );
   }
