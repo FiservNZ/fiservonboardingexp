6089cdbe09750750323d4dc5cfe5ff4057c5f50c
KT5334 <yfq5334@autuni.ac.nz>
2023-08-06 15:14:03 +1200

    Removed the phones status bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:fiservonboardingexp/screens/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fiservonboardingexp/firebase_options.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom]); //Removes the device's status bar
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
