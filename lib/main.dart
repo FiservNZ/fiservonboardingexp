2e11cea19ddd7245535bb2459a2d02d8db22190d
KT5334 <yfq5334@autuni.ac.nz>
2023-08-27 17:37:40 +1200

    created get_read_task file

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'package:fiservonboardingexp/screens/home_page.dart';
 import 'package:fiservonboardingexp/screens/login_page.dart';
 import 'package:fiservonboardingexp/screens/main_screen.dart';
 import 'package:fiservonboardingexp/theme/theme_provider.dart';
+import 'package:fiservonboardingexp/widgets/check_user.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fiservonboardingexp/firebase_options.dart';
 
 import 'package:flutter/services.dart';
-
-import 'screens/main_page.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 int? initScreen;
@@ -33,7 +32,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MainPage(),
+      home: CheckUser(),
     );
   }
 }
