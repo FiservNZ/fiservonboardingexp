7a8ca35213218c056c0eafb878c25b76fd28b748
MichaelCalbay <vjc0781@autuni.ac.nz>
2023-08-30 20:02:00 +1200

    build: moved files to diff folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fiservonboardingexp/Michael_Test/manager_view.dart';
 import 'package:fiservonboardingexp/screens/home_page.dart';
 import 'package:fiservonboardingexp/screens/login_page.dart';
 import 'package:fiservonboardingexp/screens/main_screen.dart';
