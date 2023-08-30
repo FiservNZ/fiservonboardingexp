7bfd4d1b4aa199a1b0df89e339f7fe13cacc736b
elleiryan <elle.iryan@yahoo.com>
2023-08-30 14:41:30 +1200

    Feat: testing quiz

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,32 +1,37 @@
 import 'package:fiservonboardingexp/widgets/nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../widgets/app_bar_overlay.dart';
 
 const myAppBar = AppBarOverlay();
-
-const myDrawer = Drawer(
-  backgroundColor: Colors.grey,
-  child: Column(children: [
-    DrawerHeader(child: Icon(Icons.favorite)),
-    ListTile(
-      leading: Icon(Icons.home),
-      title: Text('D A S H B O A R D'),
-    ),
-    ListTile(
-      leading: Icon(Icons.chat_bubble),
-      title: Text('M E S S A G E'),
-    ),
-    ListTile(
-      leading: Icon(Icons.settings),
-      title: Text('S E T T I N G S'),
-    ),
-    ListTile(
-      leading: Icon(Icons.logout),
-      title: Text('L O G O U T'),
-    ),
-  ]),
-);
-
 const navBar = CustomNavBar();
-const myBackgroundColor = Colors.white;
+
+// Text font design style
+const fontWeight = FontWeight.bold;
+const headerFontSize = 24.0;
+const bodyFontSize = 18.0;
+var fontStyle = GoogleFonts.quicksand();
+var headerFontStyle =
+    GoogleFonts.quicksand(fontWeight: fontWeight, fontSize: headerFontSize);
+var bodyFontStyle =
+    GoogleFonts.quicksand(fontWeight: fontWeight, fontSize: bodyFontSize);
+
+// Universal Colors
 const fiservColor = Color.fromARGB(255, 255, 102, 0);
+
+// Light Mode Colors
+var lightBackgroundColor = Colors.grey[50];
+var lightBars = Colors.grey[200];
+var lightTileColor = Colors.amber[800];
+var lightTextColor = Colors.black;
+
+// Dark Mode Colors
+// Nav/App Bar uses black & fiserv color.
+var darkBackgroundColor = Colors.blueGrey[50];
+var darkBars = Colors.black;
+var darkTileColor = Colors.black87;
+var darkTextColor = Colors.white;
+var darkHeaderFontStyle = GoogleFonts.quicksand(
+    fontWeight: fontWeight, fontSize: headerFontSize, color: darkTextColor);
+var darkBodyFontStyle = GoogleFonts.quicksand(
+    fontWeight: fontWeight, fontSize: bodyFontSize, color: darkTextColor);
---
+++ lib/util/elle_testing/screens/introduction.dart
@@ -0,0 +1,37 @@
+import 'package:fiservonboardingexp/util/constants.dart';
+import 'package:fiservonboardingexp/util/elle_testing/widgets/app_circle_button.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class IntroductionScreen extends StatelessWidget {
+  const IntroductionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Container(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: Get.width * 0.2),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.star, size: 65, color: Colors.black),
+              const SizedBox(height: 40),
+              Text(
+                "This is a quiz app.",
+                style: fontStyle,
+              ),
+              const SizedBox(height: 40),
+              AppCircleButton(
+                onTap: () => Get.offAndToNamed("/home"),
+                child: const Icon(Icons.arrow_forward, size: 35),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
