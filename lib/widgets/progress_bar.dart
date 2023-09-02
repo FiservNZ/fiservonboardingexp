dee9f1895e67667420eec0768ca3a237c8917b78
KT5334 <yfq5334@autuni.ac.nz>
2023-09-02 14:38:51 +1200

    Set up progress bar + add read task to home page

--- lib/widgets/progress_bar.dart
+++ lib/widgets/progress_bar.dart
@@ -1,22 +1,106 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import '../util/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class ProgressBar extends StatefulWidget {
-  const ProgressBar({Key? key}) : super(key: key);
+class ProgressBar extends StatelessWidget {
+  const ProgressBar({super.key});
+
+  //Method to add points
+  Future<void> addpPoints(int points) async {
+    final currentUser = FirebaseAuth.instance.currentUser!;
+    final userCollection = FirebaseFirestore.instance.collection('User');
+
+    final userDocRef = userCollection.doc(currentUser.uid);
+    final userDoc = await userDocRef.get();
+    final userMap = userDoc.data() as Map<String, dynamic>;
+
+    var curPoints = userMap['curPoints'] ?? 0;
+    var maxPoints = userMap['maxPoints'] ?? 4;
+    bool completed = userMap['categoryCompletion'] ?? false;
+
+    if (curPoints < maxPoints) {
+      curPoints += points;
+      if (curPoints == maxPoints) {
+        completed = true;
+      }
+    }
+
+    await userDocRef.update({
+      'curPoints': curPoints,
+      'maxPoints': maxPoints,
+      'categoryCompletion': completed
+    });
+  }
 
   @override
-  _ProgressBarState createState() => _ProgressBarState();
-}
-
-class _ProgressBarState extends State<ProgressBar> {
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: LinearPercentIndicator(
-          lineHeight: 20,
-          percent: 0.5,
-          progressColor: fiservColor,
-          backgroundColor: Color.fromARGB(193, 238, 192, 106)),
+    final currentUser = FirebaseAuth.instance.currentUser!;
+    final userCollection = FirebaseFirestore.instance.collection('User');
+
+    return SizedBox(
+      child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        stream: userCollection.doc(currentUser.uid).snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            final userDocument = snapshot.data!.data() as Map<String, dynamic>;
+            final curPoints = userDocument['curPoints'] ?? 0;
+            final maxPoints = (userDocument['maxPoints'] ?? 4).toInt();
+            final pointsText = '$curPoints/$maxPoints';
+
+            return Column(
+              children: [
+                SizedBox(
+                  width: 10,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Progress Bar',
+                        style: TextStyle(
+                          color: fiservColor,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        pointsText,
+                        style: const TextStyle(
+                          color: fiservColor,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 3),
+                SizedBox(
+                  width: 10,
+                  height: 10,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: LinearProgressIndicator(
+                      value: curPoints / maxPoints,
+                      valueColor: const AlwaysStoppedAnimation<Color>(
+                        fiservColor,
+                      ),
+                      backgroundColor: Colors.grey,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const Text('No data available');
+          }
+        },
+      ),
     );
   }
 }
