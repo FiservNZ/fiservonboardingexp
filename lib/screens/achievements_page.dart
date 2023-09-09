7430c2012343ab2f85e1ae7f65e5ce5a3d0fc251
persona2490 <swang2077@gmail.com>
2023-09-10 02:26:55 +1200

    add basic layout

--- lib/screens/achievements_page.dart
+++ lib/screens/achievements_page.dart
@@ -1,6 +1,7 @@
 import 'package:achievement_view/achievement_view.dart';
 import 'package:achievement_view/achievement_widget.dart';
 import 'package:flutter/material.dart';
+import '../util/category_tile.dart';
 import '../util/constants.dart';
 import '../widgets/nav_bar.dart';
 
@@ -12,16 +13,26 @@ class AchievementsPage extends StatefulWidget {
 
 class _MyCheckBoxState extends State<AchievementsPage> {
   bool isCircle = false;
+  List moduleCategories = [
+    // [categoryName, categoryImage]
+    // Orientation (First Week), Compliance, Customs and Culture, Technical training, Health &Safety
+    ["Orientation", 'assets/icon/welcome.png'],
+    ["Customs & Culture", 'assets/icon/worldwide.png'],
+    ["Compliance", 'assets/icon/compliance.png'],
+    ["Health & Safety", 'assets/icon/guidelines.png'],
+    ["Technical training", 'assets/icon/technical.png'],
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: myAppBar,
-      bottomNavigationBar: CustomNavBar(),
-      body: Column(
+      bottomNavigationBar: navBar,
+      body: ListView(
         children: <Widget>[
           Container(
             height: 200,
-            color: Color.fromARGB(255, 54, 37, 202), // 可以根据需要更改颜色
+            color: Color.fromARGB(255, 54, 37, 202),
             child: Row(
               children: [
                 Padding(
@@ -29,12 +40,31 @@ class _MyCheckBoxState extends State<AchievementsPage> {
                   child: Text(
                     "My honor",
                     style: TextStyle(
-                      color: Colors.white, // 可以根据需要更改文本颜色
+                      color: Colors.white,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              //width: double.infinity,
+              child: GridView.builder(
+                itemCount: moduleCategories.length,
+                padding: const EdgeInsets.all(12.0),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, childAspectRatio: 1 / 1.5),
+                itemBuilder: (context, index) {
+                  return CategoryTile(
+                    categoryName: moduleCategories[index][0],
+                    iconName: moduleCategories[index][1],
+                  );
+                },
+              ),
             ),
           ),
           Checkbox(
@@ -46,7 +76,6 @@ class _MyCheckBoxState extends State<AchievementsPage> {
             child: const Text("Show"),
             onPressed: () => show(context),
           ),
-          // 这里添加您现有的内容
         ],
       ),
     );
