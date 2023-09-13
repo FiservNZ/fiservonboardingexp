b874b97d67623d1ddc54f06923ff357124f48000
KT5334 <yfq5334@autuni.ac.nz>
2023-09-13 17:22:02 +1200

    added themes to the other files

--- lib/screens/menu drawer/help_page.dart
+++ lib/screens/menu drawer/help_page.dart
@@ -1,5 +1,4 @@
 import 'package:fiservonboardingexp/screens/main_screen.dart';
-import 'package:fiservonboardingexp/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
