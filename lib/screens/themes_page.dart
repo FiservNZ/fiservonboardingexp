2acdeb55351b6758757fcf6c0055495f567b6e3f
KT5334 <yfq5334@autuni.ac.nz>
2023-09-13 18:58:18 +1200

    Fixed the beach theme button

--- lib/screens/themes_page.dart
+++ lib/screens/themes_page.dart
@@ -168,7 +168,7 @@ class _ThemesPageState extends State<ThemesPage> {
                     ).merge(GoogleFonts
                         .quicksand()), // Merge styles with GoogleFonts
                   ),
-                  value: pastelTheme,
+                  value: beachTheme,
                   groupValue: selectedTheme,
                   onChanged: (value) {
                     _handleThemeChange(beachTheme, themeProvider);
