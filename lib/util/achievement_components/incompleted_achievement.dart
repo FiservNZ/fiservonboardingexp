ebb09831f762a4fd34d017a7fa372e30684ab98f
persona2490 <swang2077@gmail.com>
2023-09-13 21:26:59 +1200

    Change the design and add some png files

--- lib/util/achievement_components/incompleted_achievement.dart
+++ lib/util/achievement_components/incompleted_achievement.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../screens/achievements_page.dart';
-import 'achievement_tile.dart';
+import 'incomplete_util.dart';
 
 class IncompletedAchievement extends StatelessWidget {
   final Achievementpage _achievementpage = Achievementpage();
@@ -9,6 +9,20 @@ class IncompletedAchievement extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List SubiconList = [
+      'assets/icon/achievement/First time login!.png',
+      'assets/icon/achievement/Unlocked all themes!.png',
+      'assets/icon/achievement/Submitted feedback!.png',
+      'assets/icon/achievement/Changed Icon!.png',
+      'assets/icon/achievement/Completing checklist!.png',
+      'assets/icon/achievement/Completed orientation!.png',
+      'assets/icon/achievement/Completed Compliance!.png',
+      'assets/icon/achievement/Completed Health & Safety!.png',
+      'assets/icon/achievement/Completed Technical Training!.png',
+      'assets/icon/achievement/Completed Customs & Culture!.png',
+      'assets/icon/achievement/Completed all the modules!.png',
+      'assets/icon/achievement/Unlocked all icons!.png',
+    ];
     return FutureBuilder<List<Map<String, dynamic>>>(
       future: detectIncompletedAchi(),
       builder: (context, snapshot) {
@@ -28,16 +42,15 @@ class IncompletedAchievement extends StatelessWidget {
             children: [
               Container(
                 width: 400,
-                height: 250,
+                height: 350,
                 // color: Colors.red,
                 child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemCount: incompletedAchievements.length,
                   itemBuilder: (context, index) {
-                    return Achievement(
-                      size: 3,
+                    return IncompleteAchv(
                       title: incompletedAchievements[index]['name'],
-                      iconName: incompletedAchievements[index]['iconData'],
+                      iconName: SubiconList[index],
                       award: '',
                       isCompleted: incompletedAchievements[index]['IsComplete'],
                     );
