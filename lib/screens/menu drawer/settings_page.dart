2e45eeca196c54f847c9c8e543f3c266ce13e9b4
rlathdud1972 <rlathdud1972@gmail.com>
2023-09-16 11:49:39 +1200

    fixed squiggles

--- lib/screens/menu drawer/settings_page.dart
+++ lib/screens/menu drawer/settings_page.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fiservonboardingexp/screens/menu%20drawer/change_password_page.dart';
 import 'package:fiservonboardingexp/screens/login_page.dart';
