77cede931aa2345d60cbf12750ab73b550d02f3f
persona2490 <swang2077@gmail.com>
2023-09-15 21:37:26 +1200

    Refactor the achievement to real-time update

--- lib/util/achievement_components/incompleted_achievement.dart
+++ lib/util/achievement_components/incompleted_achievement.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import '../../firebase_references/firebase_refs.dart';
+
+import '../../firebase references/firebase_refs.dart';
 import '../../screens/achievements_page.dart';
 import 'incomplete_util.dart';
 
