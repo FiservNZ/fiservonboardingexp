a1fc7bc9b6777acc2fb81c2f79569d8fcd514737
MichaelCalbay <vjc0781@autuni.ac.nz>
2023-09-21 22:07:11 +1200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:fiservonboardingexp/controllers/read_controller.dart';
-import 'package:fiservonboardingexp/idk/read_routes.dart';
-import 'package:fiservonboardingexp/routes/quiz_test_routes.dart';
 import 'package:fiservonboardingexp/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
